dcc388fddd6944b65e6cadee9f793dc5cd22dc41
Sergey Zimakov <sergeyz@galcon.co.il>
2025-02-23 17:29:20 +0200

    about me, work experience

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
+import { aboutMeItems, workExperienceItems } from "./state.js";
+
 function toggleModalNavBar() {
   $("#modal-nav").modal('toggle');
 }
@@ -64,3 +66,69 @@ function email() {
 function linkedIn() {
   window.open('https://www.linkedin.com/in/sergey-zimakov/','_blank');
 }
+
+function fillAboutMe() {
+  const aboutMeDiv = document.getElementById('aboutMe');
+  aboutMeItems.forEach(item => {
+    const p = document.createElement('p');
+    p.innerText = item;
+    aboutMeDiv.append(p);
+  });
+}
+ 
+function fillWorkExperience() {
+  const cards = document.getElementById('work-experience-cards');
+  workExperienceItems.forEach(item => {
+    const card = document.createElement('div');
+    card.className = 'content-card';
+    if (!isEmptyString(item.position)) {
+      const positionDiv = document.createElement('div');
+      const positionText = document.createElement('span');
+      positionText.className = 'bold-text';
+      positionText.innerText = item.position;
+      positionDiv.append(positionText);
+      card.append(positionDiv);
+    }
+
+    if (!isEmptyString(item.company)) {
+      const companyDiv = document.createElement('div');
+      const companyText = document.createElement('span');
+      companyText.innerText = item.company;
+      companyDiv.append(companyText);
+      card.append(companyDiv);
+    }
+
+    if (!isEmptyString(item.dates)) {
+      const datesDiv = document.createElement('div');
+      const datesText = document.createElement('span');
+      datesText.className = 'grey-text';
+      datesText.innerText = item.dates;
+      datesDiv.append(datesText);
+      card.append(datesDiv);
+    }
+
+    if (item.description.length) {
+      const descriptionList = document.createElement('ul');
+      descriptionList.className = 'description-list';
+      item.description.forEach(di => {
+        const descItem = document.createElement('li');
+        descItem.innerText = di;
+        descriptionList.append(descItem);
+      });
+      card.append(descriptionList);
+    }
+
+    cards.append(card);
+  });
+}
+
+function isEmptyString(str) {
+  return str === '';
+}
+
+function init() {
+  fillAboutMe();
+  fillWorkExperience();
+}
+
+init();
---
+++ script/state.js
@@ -0,0 +1,39 @@
+export const workExperienceItems = [
+    {
+        position: "Backend Developer",
+        dates: "April 2023 - Present",
+        company: "Galcon",
+        description: [
+            "Developing API servers and communication servers with C# and NodeJS.",
+            "Significant improvement of load test capabilities by developing a new application that simulates running a big number of devices under different conditions.",
+            "Promoting and moving new features to test by making integration between Embedded side and Frontend.",
+            "Taking a part and responsibilities in most of the company`s projects."
+        ],
+    },
+    {
+        position: "Full Stack Developer",
+        dates: "August 2021 - March 2023",
+        company: "Catom LTD",
+        description: [
+            "Developing energy control system with ASP.Net, system and data base characterization.",
+            "Improving the timing of energy reports and bills generating by fixing and creating complex procedures and functions in SQL.",
+            "Achieving system improvement by developing new effective features.",
+            "Developing Windows Services with C#."
+        ],
+    },
+    {
+        position: "Differnt jobs at factories, like CNC Machine Operator At Iscar",
+        dates: "Until August 2021",
+        company: "",
+        description: [],
+    },
+];
+
+export const aboutMeItems = [
+    "Hi everyone,",
+    "I am backend developer with 3+ years of experience.",
+    "Living programming and learning new technologies. For me it isn't just a job, but also a favourite hobby.",
+    "Skilled in C#, API, SQL(include complex SP), ASP.Net, NodeJS, Javascript, Git.",
+    "Have basic knowledge in React, Angular, Flutter, GitHub Actions, CI/CD Proccess and Docker.",
+    "I am highly-motivated, organized, team-player and have strong self-learning skills.",
+];
